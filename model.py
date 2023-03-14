42a3e71e63b6799481f913484d281ea747c59203
Michael Burkhart <mcb93@cam.ac.uk>
2023-03-14 12:34:17 +0000

    expand the number of hyperparameters considered

--- model.py
+++ model.py
@@ -14,10 +14,18 @@ import dataset
 
 
 class GCN(t.nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        alpha_dropout=0.05,
+        gat_heads=3,
+        gat_out_channels=3,
+        dim_penultimate=5,
+    ):
         super().__init__()
-        self.gat_out_channels = 3
-        self.gat_heads = 3
+        self.alpha_dropout = alpha_dropout
+        self.gat_heads = gat_heads
+        self.gat_out_channels = gat_out_channels
+        self.dim_penultimate = dim_penultimate
         self.conv1 = GATConv(
             dataset.num_node_features,
             self.gat_out_channels,
@@ -34,10 +42,10 @@ class GCN(t.nn.Module):
             3 * self.gat_out_channels * self.gat_heads
             + dataset.num_node_features * dataset.num_nodes
             + dataset.num_graph_features,
-            5,
+            self.dim_penultimate,
         )
-        self.bnorm2 = BatchNorm1d(5)
-        self.lin2 = Linear(5, 1)
+        self.bnorm2 = BatchNorm1d(self.dim_penultimate)
+        self.lin2 = Linear(self.dim_penultimate, 1)
 
     def forward(self, x, edge_index, edge_attr, batch, graph_feats):
         x1 = t.tanh(self.conv1(x, edge_index, edge_attr))
@@ -46,7 +54,7 @@ class GCN(t.nn.Module):
         x = to_dense_batch(x, batch, max_num_nodes=dataset.num_nodes)[0]
         x = x.view(-1, x.size(1) * x.size(2))
         x = t.cat((x1, x, graph_feats), -1)
-        x = AlphaDropout(p=0.05)(x)
+        x = AlphaDropout(p=self.alpha_dropout)(x)
         x = t.tanh(self.lin1(x))
         x = self.bnorm2(x)
         x = self.lin2(x)
@@ -56,7 +64,9 @@ class GCN(t.nn.Module):
 if __name__ == "__main__":
     from torch_geometric.loader import DataLoader
 
-    self = GCN()
+    self = GCN(
+        alpha_dropout=0.1, gat_heads=4, gat_out_channels=4, dim_penultimate=4
+    )
     print(self)
 
     batch = next(
@@ -83,16 +93,16 @@ if __name__ == "__main__":
 
 """ output:
 GCN(
-  (conv1): GATConv(1, 3, heads=3)
+  (conv1): GATConv(1, 4, heads=4)
   (agg): MultiAggregation([
     MeanAggregation(),
     StdAggregation(),
     SoftmaxAggregation(learn=True),
   ], mode=cat)
-  (bnorm1): BatchNorm1d(27, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-  (lin1): Linear(in_features=97, out_features=5, bias=True)
-  (bnorm2): BatchNorm1d(5, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-  (lin2): Linear(in_features=5, out_features=1, bias=True)
+  (bnorm1): BatchNorm1d(48, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (lin1): Linear(in_features=118, out_features=4, bias=True)
+  (bnorm2): BatchNorm1d(4, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+  (lin2): Linear(in_features=4, out_features=1, bias=True)
 )
 forward function works
 """
--- tune.py
+++ tune.py
@@ -46,8 +46,14 @@ def loss_val(mdl, criterion):
 
 
 def objective(trial):
-    mdl = model.GCN()
-    optimizer = t.optim.Adagrad(
+    mdl = model.GCN(
+        alpha_dropout=trial.suggest_float("alpha_dropout", 0.0, 1e-1),
+        gat_heads=trial.suggest_int("gat_heads", 1, 5),
+        gat_out_channels=trial.suggest_int("gat_out_channels", 1, 5),
+        dim_penultimate=trial.suggest_int("dim_penultimate", 5, 25, step=5),
+    )
+    opt_type = trial.suggest_categorical("optimizer", ["Adagrad", "Adam"])
+    optimizer = getattr(t.optim, opt_type)(
         mdl.parameters(),
         lr=trial.suggest_float("lr", 1e-3, 1e-2),
         weight_decay=trial.suggest_float("wd", 1e-4, 1e-3),
@@ -56,10 +62,11 @@ def objective(trial):
 
     for epoch in range(n_epochs):
         mdl.train()
-        loader_train = t_loader.DataLoader(
-            dataset.data_train, batch_size=1000, shuffle=True
-        )
-        for data in iter(loader_train):
+        for data in iter(
+            t_loader.DataLoader(
+                dataset.data_train, batch_size=1000, shuffle=True
+            )
+        ):
             out = mdl(
                 data.x,
                 data.edge_index,
@@ -102,37 +109,37 @@ if __name__ == "__main__":
     print(study.best_value)
 
 """
-[I 2023-03-14 10:48:59,978] A new study created in memory with name: ukbb-graphs-tuning-20230314T1048Z
-[I 2023-03-14 10:51:29,419] Trial 0 finished with value: 3.624445676803589 and parameters: {'lr': 0.006187822405193944, 'wd': 0.0009578319827298369}. Best is trial 0 with value: 3.624445676803589.
-[I 2023-03-14 10:53:55,448] Trial 1 finished with value: 3.8627800941467285 and parameters: {'lr': 0.004639688241174073, 'wd': 0.0002567076530790423}. Best is trial 0 with value: 3.624445676803589.
-[I 2023-03-14 10:54:53,066] Trial 2 pruned.
-[I 2023-03-14 10:57:17,346] Trial 3 finished with value: 3.6381077766418457 and parameters: {'lr': 0.009738553844390353, 'wd': 0.00035438660215031886}. Best is trial 0 with value: 3.624445676803589.
-[I 2023-03-14 10:58:15,541] Trial 4 pruned.
-[I 2023-03-14 10:58:44,920] Trial 5 pruned.
-[I 2023-03-14 11:01:11,717] Trial 6 finished with value: 3.789339780807495 and parameters: {'lr': 0.004764868998512587, 'wd': 0.000724102559394082}. Best is trial 0 with value: 3.624445676803589.
-[I 2023-03-14 11:01:40,940] Trial 7 pruned.
-[I 2023-03-14 11:02:10,282] Trial 8 pruned.
-[I 2023-03-14 11:02:39,637] Trial 9 pruned.
-[I 2023-03-14 11:03:49,569] Trial 10 pruned.
-[I 2023-03-14 11:06:23,304] Trial 11 pruned.
-[I 2023-03-14 11:07:23,941] Trial 12 pruned.
-[I 2023-03-14 11:07:54,559] Trial 13 pruned.
-[I 2023-03-14 11:08:54,686] Trial 14 pruned.
-[I 2023-03-14 11:11:26,383] Trial 15 pruned.
-[I 2023-03-14 11:12:25,759] Trial 16 pruned.
-[I 2023-03-14 11:12:56,183] Trial 17 pruned.
-[I 2023-03-14 11:13:58,005] Trial 18 pruned.
-[I 2023-03-14 11:14:58,761] Trial 19 pruned.
-[I 2023-03-14 11:15:59,201] Trial 20 pruned.
-[I 2023-03-14 11:18:28,944] Trial 21 pruned.
-[I 2023-03-14 11:19:28,872] Trial 22 pruned.
-[I 2023-03-14 11:20:27,929] Trial 23 pruned.
-[I 2023-03-14 11:22:57,626] Trial 24 finished with value: 3.6197071075439453 and parameters: {'lr': 0.009560898430121804, 'wd': 0.0002490963395301151}. Best is trial 24 with value: 3.6197071075439453.
-[I 2023-03-14 11:23:27,899] Trial 25 pruned.
-[I 2023-03-14 11:24:27,428] Trial 26 pruned.
-[I 2023-03-14 11:25:26,710] Trial 27 pruned.
-[I 2023-03-14 11:27:55,412] Trial 28 finished with value: 3.625743865966797 and parameters: {'lr': 0.009553121819816402, 'wd': 0.00010210813146089933}. Best is trial 24 with value: 3.6197071075439453.
-[I 2023-03-14 11:28:55,047] Trial 29 pruned.
-{'lr': 0.009560898430121804, 'wd': 0.0002490963395301151}
-3.6197071075439453
+[I 2023-03-14 11:54:19,587] A new study created in memory with name: ukbb-graphs-tuning-20230314T1154Z
+[I 2023-03-14 11:55:40,891] Trial 0 finished with value: 3.8084070682525635 and parameters: {'alpha_dropout': 0.08105967709130209, 'gat_heads': 1, 'gat_out_channels': 5, 'dim_penultimate': 10, 'optimizer': 'Adagrad', 'lr': 0.004860338271132598, 'wd': 0.00012238850127290908}. Best is trial 0 with value: 3.8084070682525635.
+[I 2023-03-14 11:57:44,204] Trial 1 finished with value: 3.539700746536255 and parameters: {'alpha_dropout': 0.011248063481413318, 'gat_heads': 4, 'gat_out_channels': 5, 'dim_penultimate': 10, 'optimizer': 'Adam', 'lr': 0.00816790214807728, 'wd': 0.00040995633487050454}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:07:44,346] Trial 2 finished with value: 3.657808542251587 and parameters: {'alpha_dropout': 0.09286390844379899, 'gat_heads': 3, 'gat_out_channels': 4, 'dim_penultimate': 10, 'optimizer': 'Adagrad', 'lr': 0.008516955738167895, 'wd': 0.00039512994888576047}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:09:32,707] Trial 3 finished with value: 3.693530559539795 and parameters: {'alpha_dropout': 0.07916029000991343, 'gat_heads': 3, 'gat_out_channels': 4, 'dim_penultimate': 5, 'optimizer': 'Adam', 'lr': 0.004798612090518737, 'wd': 0.0006314490042947818}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:10:55,824] Trial 4 finished with value: 3.5682523250579834 and parameters: {'alpha_dropout': 0.02794630488094271, 'gat_heads': 2, 'gat_out_channels': 1, 'dim_penultimate': 20, 'optimizer': 'Adam', 'lr': 0.007743051502490038, 'wd': 0.00013877923918828244}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:11:19,950] Trial 5 pruned.
+[I 2023-03-14 12:12:57,635] Trial 6 finished with value: 3.5425984859466553 and parameters: {'alpha_dropout': 0.014419933111480078, 'gat_heads': 4, 'gat_out_channels': 1, 'dim_penultimate': 15, 'optimizer': 'Adam', 'lr': 0.009934788270997644, 'wd': 0.00030922777785115065}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:13:20,885] Trial 7 pruned.
+[I 2023-03-14 12:14:37,225] Trial 8 finished with value: 3.5732171535491943 and parameters: {'alpha_dropout': 0.030081956861242556, 'gat_heads': 1, 'gat_out_channels': 1, 'dim_penultimate': 25, 'optimizer': 'Adam', 'lr': 0.0064071839732298555, 'wd': 0.0001960966427014635}. Best is trial 1 with value: 3.539700746536255.
+[I 2023-03-14 12:14:52,764] Trial 9 pruned.
+[I 2023-03-14 12:15:19,130] Trial 10 pruned.
+[I 2023-03-14 12:15:53,764] Trial 11 pruned.
+[I 2023-03-14 12:16:13,557] Trial 12 pruned.
+[I 2023-03-14 12:17:18,314] Trial 13 pruned.
+[I 2023-03-14 12:18:00,834] Trial 14 pruned.
+[I 2023-03-14 12:18:17,177] Trial 15 pruned.
+[I 2023-03-14 12:18:59,701] Trial 16 pruned.
+[I 2023-03-14 12:20:19,734] Trial 17 finished with value: 3.5391483306884766 and parameters: {'alpha_dropout': 0.02242681321024366, 'gat_heads': 1, 'gat_out_channels': 3, 'dim_penultimate': 20, 'optimizer': 'Adam', 'lr': 0.007078301509604504, 'wd': 0.00015608655671708825}. Best is trial 17 with value: 3.5391483306884766.
+[I 2023-03-14 12:20:43,488] Trial 18 pruned.
+[I 2023-03-14 12:21:33,276] Trial 19 pruned.
+[I 2023-03-14 12:23:19,779] Trial 20 finished with value: 3.530109405517578 and parameters: {'alpha_dropout': 0.0011581007179390479, 'gat_heads': 5, 'gat_out_channels': 1, 'dim_penultimate': 15, 'optimizer': 'Adagrad', 'lr': 0.006268860239378637, 'wd': 0.0009620241760413245}. Best is trial 20 with value: 3.530109405517578.
+[I 2023-03-14 12:24:09,682] Trial 21 pruned.
+[I 2023-03-14 12:24:50,668] Trial 22 pruned.
+[I 2023-03-14 12:25:29,788] Trial 23 pruned.
+[I 2023-03-14 12:26:48,937] Trial 24 pruned.
+[I 2023-03-14 12:27:35,710] Trial 25 pruned.
+[I 2023-03-14 12:29:12,132] Trial 26 pruned.
+[I 2023-03-14 12:30:43,686] Trial 27 finished with value: 3.483602285385132 and parameters: {'alpha_dropout': 0.003148939363068679, 'gat_heads': 3, 'gat_out_channels': 1, 'dim_penultimate': 15, 'optimizer': 'Adam', 'lr': 0.009131823568546747, 'wd': 0.0002085365192668435}. Best is trial 27 with value: 3.483602285385132.
+[I 2023-03-14 12:32:31,030] Trial 28 finished with value: 3.540365695953369 and parameters: {'alpha_dropout': 0.0014907933174646893, 'gat_heads': 3, 'gat_out_channels': 4, 'dim_penultimate': 10, 'optimizer': 'Adam', 'lr': 0.002790039393731718, 'wd': 0.0006586332832035749}. Best is trial 27 with value: 3.483602285385132.
+[I 2023-03-14 12:33:07,566] Trial 29 pruned.
+{'alpha_dropout': 0.003148939363068679, 'gat_heads': 3, 'gat_out_channels': 1, 'dim_penultimate': 15, 'optimizer': 'Adam', 'lr': 0.009131823568546747, 'wd': 0.0002085365192668435}
+3.483602285385132
 """
